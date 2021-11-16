5573e65831bb299b1f483eac6b9ea1ee90b86b55
Minh <binhminh150301@gmail.com>
2021-11-16 12:44:26 +0700

    prac

---
+++ Bai10_Minh_575/Bai10_Minh_575/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bai10_Minh_575
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string name, department, dateOfbirth, day, language = "";
+        private int days, salary = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btInset_Click(object sender, RoutedEventArgs e)
+        {
+            name = tbxName.Text;
+            if(name.Length == 0)
+            {
+                MessageBox.Show("Lỗi: Tên nhân viên không được bỏ trống");
+            }
+            else
+            {
+                department = cbxiOz.IsSelected == true ? "Tổ chức" : (cbxiSup.IsSelected == true ? "Vật tư" : (cbxiAcc.IsSelected == true ? "Tài chính" : (cbxiPlan.IsSelected == true? "Kế hoạch" : "")));
+                if (department.Length == 0)
+                {
+                    MessageBox.Show("Lỗi: Phòng ban không được bỏ trống");
+                }
+                else
+                {
+                    if (cbxEnglish.IsChecked == true)
+                        language = "- Anh";
+                    if (cbxFrench.IsChecked == true)
+                        language = language.Length == 0 ? "- Pháp" : language + ", Pháp";
+                    if (cbxChinese.IsChecked == true)
+                        language = language.Length == 0 ? "- Trung" : language + ", Trung";
+
+                    dateOfbirth = dpkDate.Text;
+
+                    if (dateOfbirth.Length == 0)
+                    {
+                        MessageBox.Show("Lỗi: Ngày sinh không được bỏ trống");
+                    }
+                    else
+                    {
+                        day = tbxDays.Text;
+                        if(day.Length == 0)
+                        {
+                            MessageBox.Show("Lỗi: Số ngày làm việc không được bỏ trống");
+                        }
+                       else
+                        {
+                            days = int.Parse(tbxDays.Text);
+
+                            if (days > 0)
+                            {
+                                salary = days > 20 ? (2000000 + (days - 20) * 200000) : days * 100000;
+
+                                string str = name + " - " + department + language + " - " + dateOfbirth + " - " + salary;
+                                lbxShow.Items.Add(str);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Lỗi: Số ngày làm việc phải lớn hơn 0");
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+ 
+        }
+
+        private void btDel_Click(object sender, RoutedEventArgs e)
+        {
+            var today = DateTime.Today;
+
+            tbxName.Text = "";
+            tbxName.Focus();
+            cbDepartment.SelectedIndex = -1;
+            cbxEnglish.IsChecked = false;
+            cbxFrench.IsChecked = false;
+            cbxChinese.IsChecked = false;
+            dpkDate.Text = today.ToString();
+            tbxDays.Text = "";
+            lbxShow.Items.Clear();
+        }
+
+        private void btWd2_Click(object sender, RoutedEventArgs e)
+        {
+
+            Window2 w2 = new Window2(this.name,this.department,this.language,this.dateOfbirth,this.day,this.salary.ToString());
+           
+            w2.ShowDialog();
+        }
+    }
+}
---
+++ Bai10_Minh_575/Bai10_Minh_575/Window2.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bai10_Minh_575
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        private string name, depa, lang, date, days,salary;
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        public Window2(string name, string depa, string lang, string date, string days, string salary)
+        {
+            InitializeComponent();
+            this.name = name;
+            this.depa = depa;
+            this.lang = lang;
+            this.date = date;
+            this.days = days;
+            this.salary = salary;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtName.Text = name;
+            cbDepartment.Text = depa;
+            if (lang == "- Anh")
+            {
+                cbxEnglish.IsChecked = true;
+            }
+            if (lang == "-Anh, Pháp")
+            {
+                cbxEnglish.IsChecked = true;
+                cbxFrench.IsChecked = true;
+            }
+            if (lang == "-Anh, Trung")
+            {
+                cbxEnglish.IsChecked = true;
+                cbxChinese.IsChecked = true;
+            }
+            if (lang == "- Anh , Pháp, Trung")
+            {
+                cbxEnglish.IsChecked = true;
+                cbxFrench.IsChecked = true;
+                cbxChinese.IsChecked = true;
+            }
+            if (lang == "- Pháp")
+                cbxFrench.IsChecked = true;
+            if(lang == "- Pháp, Trung")
+            {
+                cbxFrench.IsChecked = true;
+                cbxChinese.IsChecked = true;
+            }    
+            if (lang.Contains("- Trung"))
+                cbxChinese.IsChecked = true;
+
+            dpkDate.Text = date;
+            tbxDays.Text = days;
+            tbxSalary.Text = salary;
+        }
+
+        private void btExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
