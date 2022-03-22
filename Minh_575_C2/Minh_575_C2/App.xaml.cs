3922b6adc83dfbef45ed2f54cff8845f90791fea
Minh <binhminh150301@gmail.com>
2022-03-22 22:44:03 +0700

    prac

--- Minh_575_C2/Minh_575_C2/App.xaml.cs
+++ Minh_575_C2/Minh_575_C2/App.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows;
 namespace Minh_575_C2
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Interaction logic for App.xaml.
     /// </summary>
     public partial class App : Application
     {
